ecd3ccb2ae7aa4156b74b5267904fab12131d991
Pedro Ortiz Gasparini <pedrogasparini99@gmail.com>
2024-06-10 16:40:37 -0300

    se crearon nuevos archivos para el inicio del proyecto

--- farmacare/src/App.jsx
+++ farmacare/src/App.jsx
@@ -1,10 +1,44 @@
+// import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+// import Protected from "./routes/Protected";
+// import NotFound from "./routes/NotFound";
+// import ProductDetails from "./components/ProductDetails/ProductDetails";
 
 const App = () => {
-
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <Protected />,
+  //     children: [
+  //       {
+  //         path: "/",
+  //         element: <Home />,
+  //       },
+  //       { path: "/product/:id", element: <ProductDetails /> },
+  //     ],
+  //   },
+  //   {
+  //     path: "/login",
+  //     element: <Login />,
+  //   },
+  //   {
+  //     path: "*",
+  //     element: <NotFound />,
+  //   },
+  // ]);
+  // return (
+  //   <div className="d-flex flex-column align-items-center">
+  //     <RouterProvider router={router} />
+  //   </div>
+  // );
 
   return (
     <div>
-      <h1>Farmacare</h1>
+      <Login />
+      <Register />
+      <Home />
     </div>
   )
 
---
+++ farmacare/src/routes/Protected.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+
+import { AuthenticationContext } from "../services/authentication/authentication.context";
+
+const Protected = () => {
+    const { user } = useContext(AuthenticationContext);
+
+    if (!user) return <Navigate to="/login" replace />;
+    return <Outlet />;
+};
+
+export default Protected;
