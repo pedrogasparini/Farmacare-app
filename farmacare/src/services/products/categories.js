37501f38adeb2978250720ab9c00dc960a976041
Lucaslovera24 <lucaslovera24@hotmail.com>
2024-06-12 14:24:14 -0300

    Creacion de carpeta products y adelanto del home

---
+++ farmacare/src/services/products/categories.js
@@ -0,0 +1,19 @@
+class CategoriesService {
+
+    categoriesList = [];
+
+    constructor() {
+        this.categoriesList = [
+            { id: 1, name: "Cuidado personal" },
+            { id: 2, name: "Medicamentos" },
+            { id: 3, name: "Productos para el cuidado de la piel" },
+            { id: 4, name: "Materiales de primeros auxilios" }
+        ]
+    }
+
+    getCategories() {
+        return this.categoriesList;
+    }
+}
+
+export default CategoriesService;
