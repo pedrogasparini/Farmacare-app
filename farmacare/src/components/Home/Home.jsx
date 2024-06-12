856dd5a68eb0af580ef2ec0b5ed6a5bc208f5429
Lucaslovera24 <lucaslovera24@hotmail.com>
2024-06-12 16:01:40 -0300

    Avance del Home

--- farmacare/src/components/Home/Home.jsx
+++ farmacare/src/components/Home/Home.jsx
@@ -10,37 +10,35 @@ const Home = () => {
     const productList = new ProductsService();
 
     const categories = categoriesService.getCategories();
-    categories.push({ id: 0, name: "Todos los productos" });
+    categories.push({ id: 0, name: "Todos" });
     categories.sort((x, y) => x.id - y.id);
 
     const products = productList.getProducts();
-    console.log(products);
-
 
     return (
-        <div>
+        <div class="home-container">
             <div class="header">
-                <nav>
+                <nav >
                     <button className="Logo">
-                        <img src="Img/logo.png" alt="logo" />
+                        <img src="src/img/logo.png" alt="logo" />
                     </button>
 
                     <button className="user">
-                        <img src="Img/user60x60.png" alt="Icono" />
+                        <img src="src/img/user60x60.png" alt="Icono" />
                     </button>
 
-                    <button className="add">
-                        <img src="Img/add57.png" alt="edit" />
+                    <button className="carrito">
+                        <img src="src/img/carrito.png" alt="carrito" />
                     </button>
                     <button className="find">
-                        <img src="Img/lupa.png" alt="lupa" />
+                        <img src="src/img/lupa.png" alt="lupa" />
                     </button>
                     <button className="turnos">
-                        <img src="Img/buscar.png" alt="buscar" />
+                        <img src="src/img/buscar.png" alt="buscar" />
                     </button>
-                    <button className="addUser">
-                        <img src="Img/addUser.png" alt="adduser" />
-                    </button>
+                    {/* <button className="addUser">
+                        <img src="src/img/addUser.png" alt="adduser" />
+                    </button> */}
                 </nav>
 
             </div>
@@ -52,8 +50,17 @@ const Home = () => {
                         ))}
                     </ul>
                 </div>
-                <div class="">
-
+                <div class="productos">
+                    {products.map((producto) => (
+                        <div key={producto.id}>
+                            <img src={producto.pathImg} alt={producto.name} />
+                            <p>{producto.name}</p>
+                            <p>{producto.price}</p>
+                            <button class="add">
+                                <img src="src/img/add40.png" alt="add" />
+                            </button>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
